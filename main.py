5001679b28b9c73bfaf75c1bdb640cb9924c4860
Edward D <eddy.the.professor@gmail.com>
2023-08-25 20:23:43 -0400

    Added the basics of pygame

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+
+pygame.init()
+
+pygame.display.set_caption("NAME | Ignition Hacks")
+pygame.display.set_icon(pygame.image.load("./Graphics/Logo.png"))
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+WIDTH = screen.get_width()
+HEIGHT = screen.get_height()
