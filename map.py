5f19ba83b5292a7b2fe434c38dc7805f8022b6bf
theWangster <mr.xywang@gmail.com>
2023-08-26 23:37:03 -0400

    added minimap

--- map.py
+++ map.py
@@ -6,26 +6,20 @@ class Map:
         self.screen = screen
 
     def update_map(self, x, y):
-        left = x - HALF_X
-        top = y - HALF_Y
+        left = x - WIDTH // 2
+        top = y - HEIGHT // 2
 
-        free_control = False
-
-        if left < 0:
-            left = 0
-            free_control = True
-        elif left + WIDTH > MAP_X:
-            left = MAP_X - WIDTH
-            free_control = True
-        if top < 0:
-            top = 0
-            free_control = True
-        elif top + HEIGHT > MAP_Y:
-            top = MAP_Y - HEIGHT
-            free_control = True
+        # if left <= 0:
+        #     left = 0
+        #     car_should_move = True
+        # elif left + WIDTH >= MAP_X:
+        #     left = MAP_X - WIDTH
+        #     car_should_move = True
+        # if top <= 0:
+        #     top = 0
+        #     car_should_move = True
+        # elif top + HEIGHT >= MAP_Y:
+        #     top = MAP_Y - HEIGHT
+        #     car_should_move = True
 
         self.screen.blit(MAP, (0, 0), (left, top, WIDTH, HEIGHT))
-
-        return free_control
-
-
